cc5967ae6d0a3525a53f85e8344ad8a71ebeb079
MattiaR <rovitomattia91@gmail.com>
2021-06-21 15:10:03 +0200

    add primo step

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+//Todo Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+//Todo Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
 const app = new Vue ({
 
+    el : '#app',
 
-    
+    data : {
+        messaggio : "Primo esercizio Vue!",
+        colore : "red"
+    }
+
 });
